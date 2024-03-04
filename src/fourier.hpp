532f7e30cb4fa8bf57e6b3e5e3573da92b3cc179
Prady <pradyumnkejriwal1@gmail.com>
2024-03-04 17:46:33 +0530

    Modified declaration of fourier_series

--- src/fourier.hpp
+++ src/fourier.hpp
@@ -3,6 +3,10 @@
 
 using namespace std;
 
-vector<Vector2D> fourier_series(vector<Vector2D> points){
+/**
+ * @param numFrequenciees if this is k then the return value will have size 2*k+1
+*/
+vector<Vector2D> fourier_series(vector<Vector2D> points, int numFrequencies)
+{
     
 }
