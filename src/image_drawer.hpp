86cc5a6dff7631c3432982396f66997a2fc00980
Prady <pradyumnkejriwal1@gmail.com>
2024-03-04 14:12:54 +0530

    Made image_drawer prototype

---
+++ src/image_drawer.hpp
@@ -0,0 +1,27 @@
+#pragma once
+#include "Vector2D.hpp"
+#include <vector>
+
+using namespace std;
+
+class ImageDrawer{
+    ImageDrawer();
+    void startDrawing()
+    {
+
+    }
+    void endDrawing() // ends the drawing and saves the points in a file
+    {
+
+    }
+    void insertPoint(Vector2D point){
+
+    }
+    vector<Vector2D> loadDrawing(int id){
+
+    }
+    vector<pair<int, string>> getDrawings() //returns the currently saved drawings
+    {
+
+    }
+};
