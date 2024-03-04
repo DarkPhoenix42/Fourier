ba96539ae250308fad98f4a3a60e8153c33435b6
Prady <pradyumnkejriwal1@gmail.com>
2024-03-04 17:54:34 +0530

    Updated fourier_series function

--- src/fourier.hpp
+++ src/fourier.hpp
@@ -5,8 +5,9 @@ using namespace std;
 
 /**
  * @param numFrequenciees if this is k then the return value will have size 2*k+1
+ * @param samplingRate the dt in the integration will be 1/samplingRate
 */
-vector<Vector2D> fourier_series(vector<Vector2D> points, int numFrequencies)
+vector<Vector2D> fourier_series(vector<Vector2D> points, int numFrequencies, int samplingRate)
 {
     
 }
